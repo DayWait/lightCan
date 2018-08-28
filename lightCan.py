ec044d481a567d06d5cc475c66ce709170467e42
Day Wait <DayWait@users.noreply.github.com>
2018-08-28 15:27:40 -0700

    Comment and slight changes
    I added comments, and changed the cleaning part to be more versatile. Also, I got rid of replacing invalid characters with a space. if you had the word "!!!!!!!!!!!!!!!!Hello", it would return "                 (Hello in morse code)".

--- lightCan.py
+++ lightCan.py
@@ -1,32 +1,39 @@
-def getMorse(a):
-	
-	a = a.lower()	
+def getMorse(word):
+    """
+    Converts a word to morse code and returns the morse code. Ignores Non-Alphanumeric characters
+    :param word: User input word
+    :return: A word converted to morse code
+    """
 
-	morseCode = ""
+    # Dictionary/Key List to convert alphanumeric to morse code
+    morseCodeDictionary = {
+        # Digits
+        '1': '. - - - - /', '2': '. . - - - /', '3': '. . . - - /',
+        '4': '. . . . - /', '5': '. . . . . /', '6': '- . . . . /',
+        '7': '- - . . . /', '8': '- - - . . /', '9': '- - - - . /',
+        '0': '- - - - - /',
 
-	morseCodeDictionary = {
-		'1': '. - - - - /', '2': '. . - - - /', '3': '. . . - - /',
-		'4': '. . . . - /', '5': '. . . . . /', '6': '- . . . . /',
-		'7': '- - . . . /', '8': '- - - . . /','9' : '- - - - . /',
-		'0': '- - - - - /',
+        # Alphabets
+        'a': '. - /', 'b': '- . . . /', 'c': '- . - . /', 'd': '- . . /',
+        'e': '. /', 'f': '. . - . /', 'g': '- - . /', 'h': '. . . . /',
+        'i': '. . /', 'j': '. - - - /', 'k': '- . - /', 'l': '. - . . /',
+        'm': '- - /', 'n': '- . /', 'o': '- - - /', 'p': '. - - . /',
+        'q': '- - . - /', 'r': '. - . /', 's': '. . . /', 't': '- /',
+        'u': '. . - /', 'v': '. . . - /', 'w': '. - - /', 'x': '- . . - /',
+        'y': '- . - - /', 'z': '- - . . /',
+    }
 
-		'a': '. - /', 'b': '- . . . /', 'c': '- . - . /', 'd': '- . . /',
-		'e': '. /', 'f': '. . - . /', 'g': '- - . /', 'h': '. . . . /',
-		'i': '. . /', 'j': '. - - - /', 'k': '- . - /', 'l': '. - . . /',
-		'm': '- - /', 'n': '- . /', 'o': '- - - /', 'p': '. - - . /',
-		'q': '- - . - /', 'r': '. - . /', 's': '. . . /', 't': '- /',
-		'u': '. . - /', 'v': '. . . - /', 'w': '. - - /', 'x': '- . . - /',
-		'y': '- . - - /', 'z': '- - . . /',
-	}
+    word = word.lower()  # Convert user input into lowercase. There are no uppercase morse code
+    morseCode = ""  # Holds the character converted to morse code
 
-	ignore = "`~!@#$%^&*()_+-=\\][|}{\';\":/.,?>< "
+    # Converts only alphanumeric letters to morse code
+    for letter in word:
+        if letter.isalnum():
+            morseCode += morseCodeDictionary[letter]
 
-	for letter in a:
-		if letter in ignore:
-			morseCode += " "
-		else:
-			morseCode += str(morseCodeDictionary[letter])
-	return morseCode
+    return morseCode  # Returns the word that has been turned into morse code
 
+
+# Test code
 userInput = input("Enter message: ")
 print(getMorse(userInput))
